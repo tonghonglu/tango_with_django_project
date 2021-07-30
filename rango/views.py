af8da49aad7b7036357ae182b6400e7a8f87dd29
tonghonglu <87977711+tonghonglu@users.noreply.github.com>
2021-07-30 20:43:23 +0800

    ch10

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 
 def index(request):
@@ -17,12 +18,17 @@ def index(request):
     context_dict['pages'] = page_list
 
 
+    visitor_cookie_handler(request)
+
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -63,7 +69,6 @@ def add_page(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
     except:
         category = None
-
 
     if category is None:
         return redirect(reverse('rango:index'))
@@ -147,3 +152,24 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits

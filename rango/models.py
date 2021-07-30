20dd8daf834d2a3eca0a79d6408bf6122a892404
tonghonglu <87977711+tonghonglu@users.noreply.github.com>
2021-07-30 09:30:31 +0800

    Update models.py

--- rango/models.py
+++ rango/models.py
@@ -5,9 +5,6 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
-    class Meta:
-        verbose_name_plural = 'Categories'
-
     def __str__(self):
         return self.name
 
